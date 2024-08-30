8e46fe97eea612abb325159bce152a625ca3a983
akshxy12 <akshxy12@gmail.com>
2024-08-31 02:33:49 +0300

    Completed the first problem of Advent of Code 2023

--- 2023/day-1/c/main.c
+++ 2023/day-1/c/main.c
@@ -1,53 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
-#define INPUT_FILE "input_test.txt"
+#define INPUT_FILE "input.txt"
 #define BUFFER_SIZE 100
+#define CALIBRATION_VALUE_BUFFER_SIZE 3
 
 int main()
 {
-    FILE *input_buf = fopen(INPUT_FILE, "r");
+    FILE* input_file = fopen(INPUT_FILE, "r");
     char buffer[BUFFER_SIZE];
-    char number_string[3];
-    int number_string_index = -1;
-    int extracted_number = 0;
-    int sum = 0;
+    char calib_value_buffer[CALIBRATION_VALUE_BUFFER_SIZE];
+    int calib_value_buffer_index = 0;
+    int sum_calib_vals = 0;
     int i = 0;
     int line_num = 0;
 
-    number_string[2] = '\0';
+    for(line_num = 1; fgets(buffer, BUFFER_SIZE, input_file) != NULL; line_num++) {
+        printf("==Line %d== Buffer: %s", line_num, buffer);
+        calib_value_buffer_index = -1;
 
-    while (fgets(buffer, BUFFER_SIZE, input_buf))
-    {
-        number_string_index = -1;
-        ++line_num;
-        printf("==Line %d== ", line_num);
-
-        for (int i = 0; buffer[i] != '\n' && buffer[i] != EOF && buffer[i] != '\0'; i++)
-        {
+        for(i = 0; buffer[i] != '\n' && buffer[i] != '\0' && buffer[i] != EOF; i++) {
             if(isdigit(buffer[i])) {
-                if(i > 0)
-                    printf(" ");
-                printf("[%c] ", buffer[i]);
-            } else {
-                printf("%c", buffer[i]);
+                if(calib_value_buffer_index <= 0) {
+                    calib_value_buffer[++calib_value_buffer_index] = buffer[i];
+                } else {
+                    calib_value_buffer[calib_value_buffer_index] = buffer[i];
+                }
             }
         }
 
-        printf(",\t0: %c, 1: %c\n", number_string[0], number_string[1]);
-        if(number_string_index == 1) {
-            printf("#### number_string_index is 1.\n");
-            number_string[1] = number_string[0];
+        if(calib_value_buffer_index == 0) {
+            calib_value_buffer[1] = calib_value_buffer[0];
         }
-        
-        extracted_number = atoi(number_string);
-        printf(" ==> %d\n", extracted_number);
 
-        sum += extracted_number;
-   }
+        calib_value_buffer[2] = '\0';
+        sum_calib_vals += atoi(calib_value_buffer);
+        printf("Calibration value: %s\n", calib_value_buffer);
+        printf("\n");
+    }
 
-    printf("Sum: %d\n", sum);
-    fclose(input_buf);
+    printf("Sum: %d\n", sum_calib_vals);
+
+    fclose(input_file);
     return 0;
 }
